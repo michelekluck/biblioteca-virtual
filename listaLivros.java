bebac47ec7d814bb06724e073e68b1f530f6dfa0
michelekluck <michelenamur27@gmail.com>
2025-03-11 23:42:09 -0300

    criando linkedlist de livros

---
+++ listaLivros.java
@@ -0,0 +1,19 @@
+import java.util.LinkedList;
+
+public class listaLigada {
+    public static void main(String[] args) {
+            LinkedList<String[]> linkedList= new LinkedList<>();
+
+            linkedList.add(new String[] {"Trono de vidro", "Sarah J. Maas", "2013"});
+            linkedList.add(new String[]{"O povo brasileiro", "Darcy Ribeiro", "2015"});
+            linkedList.add(new String[]{"Pessoas normais", "Sally Rooney", "2019"});
+
+            printLinkedList(linkedList);
+        }
+
+        public static void printLinkedList(LinkedList<String[]> linkedList) {
+        for (String[] infoLivro : linkedList) {
+            System.out.println(String.join(" | ", infoLivro));
+        }
+    }
+}
